7c8ab664522123c64f86cce925e092e7c996450e
Debra-Jin <54689219+Debra-Jin@users.noreply.github.com>
2022-03-21 22:19:46 -0400

    ruoxi changes

---
+++ Scene0.js
@@ -0,0 +1,32 @@
+class Scene0 extends Phaser.Scene {
+    constructor () {
+        super('Scene0');
+    }
+
+    preload () {}
+
+    create () {
+        var text_style = {
+            font: 'bold 32px Arial',
+            color: '#fff'
+        }
+        var centerX = this.physics.world.bounds.centerX
+
+        var text_title1 = this.add.text(centerX - 100, 200, "It's Raining", text_style)
+        var text_title2 = this.add.text(centerX - 100, 300, "It's Choring", text_style)
+
+        var text_start = this.add.text(100, 700, 'Start', text_style)
+        text_start.setInteractive()
+        text_start.on('pointerdown', function (pointer) {
+            // start => move to the next screen
+            this.scene.start('Scene1')
+        }, this)
+
+        var text_exit = this.add.text(600, 700, 'Exit', text_style)
+        text_exit.setInteractive()
+        text_exit.on('pointerdown', function (pointer) {
+            // exit game
+            this.scene.stop('Scene0')
+        })
+    }
+}
--- game.js
+++ game.js
@@ -5,6 +5,15 @@ var config = {
     backgroundColor: "0x000000",
     scene:[Scene1,Scene2],
     pixelArt:true,
+    physics: {
+        default: 'arcade',
+            arcade: {
+            // gravity: {
+            //     y: 200
+            // },
+        debug: true
+        }
+    }
 
 }
 
